f8e0a08d0a9bae316d7638b02b85d73352eb6e3d
Mani Movassagh <74688476+manimovassagh@users.noreply.github.com>
2023-07-24 16:57:13 +0200

    Add files via upload

--- mani-social-app/social-app-server/src/main/java/com/github/manimovassagh/entities/Tweet.java
+++ mani-social-app/social-app-server/src/main/java/com/github/manimovassagh/entities/Tweet.java
@@ -1,52 +1,29 @@
 package com.github.manimovassagh.entities;
 
 import io.quarkus.hibernate.orm.panache.PanacheEntity;
+import jakarta.ejb.Local;
 import jakarta.persistence.Entity;
+import jakarta.persistence.PrePersist;
+import lombok.*;
 
+import java.time.LocalDate;
 import java.time.LocalDateTime;
 
+
 @Entity
+@Setter
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
 public class Tweet extends PanacheEntity {
 
     public String tweet;
     public String username;
+    public LocalDate createdDate;
 
 
-
-    public Tweet(String tweet, String userName, LocalDateTime createdAt) {
-        this.tweet = tweet;
-        this.username = userName;
-
-    }
-
-    public Tweet() {
-    }
-
-    public String getTweet() {
-        return tweet;
-    }
-
-    public void setTweet(String tweet) {
-        this.tweet = tweet;
-    }
-
-    public String getUsername() {
-        return username;
-    }
-
-    public void setUsername(String userName) {
-        this.username = userName;
-    }
-
-
-
-    @Override
-    public String toString() {
-        return "Tweet{" +
-                "tweet='" + tweet + '\'' +
-                ", userName='" + username + '\'' +
-
-                ", id=" + id +
-                '}';
+    @PrePersist
+    public void addDate() {
+        this.createdDate = LocalDate.now();
     }
 }
